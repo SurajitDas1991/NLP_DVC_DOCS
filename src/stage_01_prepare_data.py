386cd5758ed910d0b0ea8a3d0171df9c3d759df6
Surajit Das <dsurajitd@gmail.com>
2022-06-21 23:48:03 +0530

    Added source files

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -4,6 +4,7 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
+from src.utils.data_mgmt import process_posts
 import random
 
 
@@ -21,8 +22,31 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
 
+    source_data_dir=config["source_data"]["data_dir"]
+    source_data_file=config["source_data"]["data_file"]
+    source_data_path=os.path.join(source_data_dir,source_data_file)
+
+    split=params["prepare"]["split"]
+    random_seed=params["prepare"]["seed"]
+    tag=params["prepare"]["tag"]
+
+    random.seed(random_seed)
+
+    artifacts=config["artifacts"]
+    prepared_data_dir_path=os.path.join(artifacts["ARTIFACTS_DIR"], artifacts['PREPARED_DATA'])
+    create_directories([prepared_data_dir_path])
+
+    train_data_path=os.path.join(prepared_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path=os.path.join(prepared_data_dir_path,artifacts["TEST_DATA"])
+    #create_directories([train_data_path,test_data_path])
+
+
+    encode="utf8"
+    with open(source_data_path, encoding=encode) as fd_in: # actual input data that we are reading
+        with open(train_data_path, "w", encoding=encode) as fd_out_train: # writing train data
+            with open(test_data_path, "w", encoding=encode) as fd_out_test: # writing test data
+                process_posts(fd_in, fd_out_train, fd_out_test, tag, split)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
